456af9c80e03bb75b10b3be7d4076f3f80985d9b
EvangelosGeorgiou <tagishav@hotmail.com>
2021-03-16 22:01:17 +0200

    Update Body parts

--- database/seeds/BodyPartsSeeder.php
+++ database/seeds/BodyPartsSeeder.php
@@ -17,11 +17,11 @@ class BodyPartsSeeder extends Seeder
         // Male organs, male skeleton, female skeleto: en eshi wrist
         $bodyParts = [
             'Skull', 'Teeth Top', 'Teeth Bottom', 'Jaw', 'Spine', 'Clavicle Left', 'Clavivle Right', 'Scapula Left', 'Scapula Right', 'Humerus Left', 'Humerus Right', 'Radius Left', 'Radius Right', 'Ulna Left', 'Ulna Right',
-            'Palm Left','Palm Right', 'Ribs', 'Sternum', 'Pelvic', 'Thigh Left', 'Thigh Right', 'Knee Left', 'Knee Right', 'Fibula Right', 'Fibula Left', 'Tibia Right', 'Tibia Left', 'Foot Left', 'Foot Right','Left Wrist','Right Wrist',
+            'Wrist Left','Wrist Right', 'Ribs', 'Sternum', 'Pelvic', 'Thigh Left', 'Thigh Right', 'Knee Left', 'Knee Right', 'Fibula Right', 'Fibula Left', 'Tibia Right', 'Tibia Left', 'Foot Left', 'Foot Right','Left Forearms','Right Forearms',
 
             'Brain','Eye Left','Eye Right','Lungs','Heart','Stomach','Liver','Pancreas','Kidney' ,'Large Intestine' ,'Small Instestine' ,'Urinary' ,'Pelvic' ,
 
-            'Head', 'Neck', 'Traps', 'Latissimus Dorsi', 'Rhomboid', 'Left Shoulder', 'Right Shoulder', 'Left Tricep', 'Right Tricep', 'Left Bicep', 'Right Bicep', 'Left Forearm', 'Right Foream', 'Chest',
+            'Neck', 'Traps', 'Latissimus Dorsi', 'Left Shoulder', 'Right Shoulder', 'Left Tricep', 'Right Tricep', 'Left Bicep', 'Right Bicep', 'Left Forearm', 'Right Foream', 'Chest',
             'Abs', 'Glutes', 'Left Abductors','Right Abductors', 'Left Hamstring', 'Right Hamstring', 'Left Quadriceps', 'Right Quadriceps', 'Left Calves', 'Right Calves', '', '', '', '',
         ];
 
